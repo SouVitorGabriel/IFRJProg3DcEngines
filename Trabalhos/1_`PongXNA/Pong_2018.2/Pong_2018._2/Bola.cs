ceff31c004d04355c986e5da4d09f5ad1365d9ba
SouVitorGabriel <vitor.s.alencar@hotmail.com>
2018-09-06 11:38:21 -0300

    Pasta organizada

---
+++ Trabalhos/1_`PongXNA/Pong_2018.2/Pong_2018._2/Bola.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong_2018._2
+{
+    class Bola
+    {
+        float x, y, speedX, speedY;
+        public Texture2D textura { get; set; }
+        Game game;
+        public Bola(float x, float y, int speedX, float speedY, Game game)
+        {
+            this.x = x;
+            this.y = y;
+            this.speedX = speedX;
+            this.speedY = speedY;
+            this.game = game;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+    }
+}
---
+++ Trabalhos/2_MoonPatrolXNA/MoonPatrolXNA/MoonPatrolXNA/MoonPatrolXNA/GameObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MoonPatrolXNA
+{
+    class GameObject
+    {
+        protected Point position;
+        public Point Position { get => position; set { position = value; collider.X = position.X - collider.Width / 2; collider.Y = position.Y - collider.Height / 2; } }
+
+        protected Rectangle collider;
+        public Rectangle Collider { get => collider; }
+
+        protected Texture2D texture;
+
+      
+        public GameObject(ContentManager content, string texturePath, Point position, Point size)
+        {
+            texture = content.Load<Texture2D>(texturePath);
+            this.position = position;
+            collider = new Rectangle(position.X - size.X / 2, position.Y - size.Y / 2, size.X, size.Y);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, collider, Color.White);
+        }
+        
+        
+        
+
+        public virtual void OnColision()
+        {
+
+        }
+    }
+}
